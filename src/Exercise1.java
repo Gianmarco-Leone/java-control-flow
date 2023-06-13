f30643e212be630439e4b8bcaab2022962726e3e
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-06-13 17:45:34 +0200

    add ex1

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -9,5 +9,30 @@
 // etc…
 // 3. Do it again but use only 4 lines of code (not including method and curly braces of method.
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Exercise1 {
+    public static void main(String[] args) {
+        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+//        for (int i = 0; i < daysOfWeek.length; i++) {
+//            System.out.println(daysOfWeek[i]);
+//        }
+
+//        for (String day: daysOfWeek) {
+//            System.out.println(day);
+//        }
+
+//        for (int i = 0; i < daysOfWeek.length; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(daysOfWeek[i]);
+//            } else {
+//                System.out.println(daysOfWeek[i].toUpperCase());
+//            }
+//        }
+
+        for (int i = 0; i < daysOfWeek.length; i++) {
+            String text = (i % 2 == 0) ? (daysOfWeek[i]) : daysOfWeek[i].toUpperCase();
+            System.out.println(text);
+        }
+    }
 }
